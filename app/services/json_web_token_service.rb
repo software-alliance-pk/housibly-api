afb1718b18bfe8b6083c08b9622bb9dd97d1799f
ror-vs <backend@viralsquare.org>
2022-07-13 19:31:37 +0500

    Shift credentials function is added in json service

--- app/services/json_web_token_service.rb
+++ app/services/json_web_token_service.rb
@@ -1,10 +1,19 @@
 class JsonWebTokenService
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode payload, Rails.application.secrets.secret_key_base, 'HS256'
+    JWT.encode payload, shift_credentials, 'HS256'
   end
 
   def self.decode(token)
-    JWT.decode(token, Rails.application.secrets.secret_key_base, algorithm: 'HS256')[0]
+    JWT.decode(token, shift_credentials, algorithm: 'HS256')[0]
   end
+
+  def self.shift_credentials
+    if Rails.env.production?
+      Rails.application.secret_key_base
+    else
+      Rails.application.secrets.secret_key_base
+    end
+  end
+
 end

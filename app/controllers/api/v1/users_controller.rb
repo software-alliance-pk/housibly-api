ddf4167d224c4d45187278b427ddd1ed2c791562
ror-vs <backend@viralsquare.org>
2022-07-14 21:37:08 +0500

    validation false for update_profile api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,8 +4,9 @@ class Api::V1::UsersController < Api::V1::ApiController
   end
 
   def update_profile
-    if @current_user.valid?
-      if @current_user.update(user_params)
+    if @current_user.present?
+      @current_user.assign_attributes(user_params)
+      if @current_user.save(validate: false)
         @current_user
       else
         render_error_messages(@current_user)

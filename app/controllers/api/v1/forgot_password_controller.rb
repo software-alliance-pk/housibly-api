fb67d4efd656e9d999844c65dd9604d8173fa095
ror-vs <backend@viralsquare.org>
2022-07-14 14:54:48 +0500

    reset_password_with_phone create api

--- app/controllers/api/v1/forgot_password_controller.rb
+++ app/controllers/api/v1/forgot_password_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::ForgotPasswordController < Api::V1::ApiController
   skip_before_action :authenticate_user
   include CreateOtp
+
   def forgot_password_through_email
     if user_params[:email].blank?
       return render json: { message: 'email not found' }
     end
     user = User.find_by(email: user_params[:email])
     if user.present?
-      otp(user)
+      signup_otp(user)
 
       # user.generate_password_token! #generate pass token
       # # SEND EMAIL/PHONE HERE
@@ -16,38 +17,54 @@ class Api::V1::ForgotPasswordController < Api::V1::ApiController
       render json: { message: ['Email address not found. Please check and try again.'] }, status: :not_found
     end
   end
+
   def forgot_password_through_phone
     if user_params[:phone_number].blank?
       return render json: { message: 'phone number is not found' }
     end
     user = User.find_by(phone_number: user_params[:phone_number])
     if user.present?
-      otp(user)
-      # user.generate_password_token! #generate pass token
-      # # SEND EMAIL HERE
-      # render json: { "otp": user.reset_password_token}, status: :ok
+      signup_otp(user)
     else
       render json: { message: ['Phone number is not found. Please check and try again.'] }, status: :not_found
     end
   end
-  def reset_password
-    otp = user_params[:otp].to_s
-    return render json: {message: "OTP is blank"} if user_params[:otp].blank?
-    return render json: {message: "Email/Phone is blank"}  if user_params[:email].blank? && user_params[:phone_number].blank?
-    user = User.find_by(reset_password_token: otp)
-    if user.present? && user.password_token_valid?
+
+  def reset_password_with_email
+    # otp = user_params[:otp].to_s
+    # return render json: { message: "OTP is blank" } if user_params[:otp].blank?
+    return render json: { message: "Email is blank" } if user_params[:email].blank?
+    # user = User.find_by(reset_password_token: otp)
+    user = User.find_by(email: user_params[:email])
+    # if user.present? && user.password_token_valid?
+    if user.present?
       if user.reset_password!(user_params[:password])
         render json: { status: 'password changed successfully' }, status: :ok
       else
         render json: { message: user.messages.full_message }, status: :unprocessable_entity
       end
     else
-      render json: { message: ['Link not valid or expired. Try again'] }, status: :not_found
+      render json: { message: "Email not found" }, status: :not_found
+    end
+  end
+
+  def reset_password_with_phone
+    return render json: { message: "Phone is blank" } if user_params[:phone_number].blank?
+    user = User.find_by(phone_number: user_params[:phone_number])
+    if user.present?
+      if user.reset_password!(user_params[:password])
+        render json: { status: 'password changed successfully' }, status: :ok
+      else
+        render json: { message: user.messages.full_message }, status: :unprocessable_entity
+      end
+    else
+      render json: { message: 'Phone not found' }, status: :not_found
     end
   end
 
   private
+
   def user_params
-    params.require(:user).permit(:email, :password ,:phone_number, :otp)
+    params.require(:user).permit(:email, :password, :phone_number)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
             :profile_type, :password_digest, presence: true
   validates_inclusion_of :contacted_by_real_estate, :licensed_realtor, in: [true, false]
   validates :phone_number, format: { with: /\A^\+?\d+$\z/ }
+  validates :phone_number, uniqueness: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :password_digest, length: { minimum: 6 }, confirmation: true
   validates :user_type, inclusion: { in: %w(seller buyer neither) }
@@ -31,14 +32,14 @@ class User < ApplicationRecord
     self.save!
   end
 
+  def password_token_valid?
+    (self.reset_password_sent_at + 4.hours) > Time.now.utc
+  end
+
   def generate_signup_token!
     self.reset_signup_token = generate_otp
     self.reset_signup_token_sent_at = Time.now.utc
     self.save!
-  end
-
-  def password_token_valid?
-    (self.reset_password_sent_at + 4.hours) > Time.now.utc
   end
 
   def signup_token_valid?
@@ -46,7 +47,7 @@ class User < ApplicationRecord
   end
 
   def reset_password!(password)
-    self.reset_password_token = nil
+    self.reset_signup_token = nil
     self.password = password
     self.save!
   end

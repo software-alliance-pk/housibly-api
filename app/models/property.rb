5d2c55ced90fbd90e521be7d4dcd6c122d895beb
ror-vs <backend@viralsquare.org>
2022-08-01 16:32:33 +0500

    Validations changed

--- app/models/property.rb
+++ app/models/property.rb
@@ -7,10 +7,12 @@ class Property < ApplicationRecord
   validates :lot_frontage_unit, :currency_type, :lot_depth_unit, presence: true
   validates :bath_rooms, presence: true, unless: ->(property){property.property_type == "vacant_land"}
   validates :bed_rooms, presence: true, unless: ->(property){property.property_type == "vacant_land"}
-  validates :house_type, presence: true, unless: ->(property){property.property_type == "vacant_land"}
-  validates :house_style, presence:  true, unless: ->(property){property.property_type == "vacant_land"}
+  validates :house_type, presence: true, unless: ->(property){property.property_type == "vacant_land" || "condo"}
+  validates :house_style, presence:  true, unless: ->(property){property.property_type == "vacant_land" || "condo"}
   validates :air_conditioner, presence:  true, unless: ->(property){property.property_type == "vacant_land"}
   validates :parking_spaces, presence: true, unless: ->(property){property.property_type == "vacant_land"}
   validates :garage_spaces, presence: true, unless: ->(property){property.property_type == "vacant_land"}
+  validates :condo_type, presence: true, unless: ->(property){property.property_type == "vacant_land" || "house"}
+  validates :condo_style, presence: true, unless: ->(property){property.property_type == "vacant_land" || "house"}
 
 end

e48a5d28d7ce5cbe3f06b5757f1c2f6b8483b2bc
ror-vs <backend@viralsquare.org>
2022-08-04 16:04:50 +0500

    Remove puts

--- app/services/social_login_service.rb
+++ app/services/social_login_service.rb
@@ -23,14 +23,7 @@ class SocialLoginService
     response = Net::HTTP.get_response(uri)
     return JSON.parse(response.body) if response.code != '200'
     json_response = JSON.parse(response.body)
-    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    puts json_response['email']
-    puts json_response['sub']
-    puts json_response
-    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    user = create_user(json_response['email'], json_response['sub'], json_response)
+    user = create_user!(json_response['email'], json_response['sub'], json_response)
     puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
     puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
     puts user

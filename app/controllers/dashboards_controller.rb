7817b1cf9bf10b5e753c91c59808906e900dc11b
vs-ror <backend@viralsquare.org>
2022-08-11 22:37:29 +0900

    Add pagination in the dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,10 @@
 class DashboardsController < ApplicationController
   def index
     unless params[:search].blank?
-      @new_users = User.new_users
-      @new_users = new_user.custom_search(params[:search])
+      @new_users = User.new_users.paginate(page: params[:page], per_page: 1)
+      @new_users = new_user.custom_search(params[:search]).paginate(page: params[:page], per_page: 1)
     else
-      @new_users = User.new_users
+      @new_users = User.new_users.paginate(page: params[:page], per_page: 1)
     end
     
   end

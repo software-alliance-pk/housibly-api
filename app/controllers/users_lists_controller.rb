7c716fb11bd9fd480301e82c386341379ff80296
vs-ror <backend@viralsquare.org>
2022-08-16 17:23:07 +0900

    user update account

--- app/controllers/users_lists_controller.rb
+++ app/controllers/users_lists_controller.rb
@@ -2,7 +2,7 @@ class UsersListsController < ApplicationController
   skip_before_action :authenticate_admin!
   def index
     unless params[:search].blank?
-      @all_users = User.all_users.custom_search(params[:search]).paginate(page: params[:page], per_page: 3)
+      @all_users = User.all_users.custom_search(params[:search]).paginate(page: params[:page], per_page: 10)
       respond_to do |format|
         format.html
         format.csv { send_data @all_users.to_csv }
@@ -16,8 +16,24 @@ class UsersListsController < ApplicationController
     end
   end
 
-  def update
-
+  def active_user
     @user = User.find_by(id: params[:id])
+      if @user.update(active: true)
+      redirect_to users_lists_path
+    else
+      redirect_to users_lists_path
+
+    end
   end
+
+
+  def deactive_user
+     @user = User.find_by(id: params[:id])
+      if @user.update(active: false)
+      redirect_to users_lists_path
+    else
+      redirect_to users_lists_path
+    end
+  end
+
 end

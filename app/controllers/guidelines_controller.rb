1a3adaa7f25037bf38970f1d48661323bac6c9a9
vs-ror <backend@viralsquare.org>
2022-08-17 16:10:56 +0900

    guidelines

--- app/controllers/guidelines_controller.rb
+++ app/controllers/guidelines_controller.rb
@@ -1,5 +1,10 @@
 class GuidelinesController < ApplicationController
   def index
-    
+    @page = Page.find_by(permalink: params[:permalink].blank? ? "terms&condition" : params[:permalink])
+  end
+
+  def guidelines
+    @page = Page.find_by(permalink: params[:permalink])
+    render 'index'
   end
 end

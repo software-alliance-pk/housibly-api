8f7a89c558f77c18cd3a957a78c9c0e51b285490
vs-ror <backend@viralsquare.org>
2022-09-02 00:30:34 +0900

    production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -130,4 +130,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.url = 'wss://housibly.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 end

6c852df4cf8b77157d9a505532ea1f8b2dcd33be
vs-ror <backend@viralsquare.org>
2022-09-02 17:48:58 +0900

    new channel

--- app/channels/support_conversation_channel.rb
+++ app/channels/support_conversation_channel.rb
@@ -1,19 +1,17 @@
-class ConversationChannel < ApplicationCable::Channel
+class SupportConversationChannel < ApplicationCable::Channel
   def subscribed
-    stop_all_streams
-    Conversation.where(sender_id: current_user).or(Conversation.where(recipient_id: current_user)).find_each do |conversation|
-      stream_from "conversations_#{conversation.id}"
-    end
-  end
+ stop_all_streams
+    SupportConversation.where(sender_id: current_user).or(SupportConversation.where(recipient_id: current_user)).find_each do |conversation|
+      stream_from "conversations_#{conversation.id}"  end
 
   def unsubscribed
     stop_all_streams
   end
 
   def receive(data)
-    @conversation = Conversation.find_by(id: data.fetch("conversation_id"))
+    @conversation = SupportConversation.find_by(id: data.fetch("conversation_id"))
     if (@conversation.sender_id == current_user.id) || (@conversation.recipient_id == current_user.id)
-      message = @conversation.messages.build(user_id: current_user.id)
+      message = @conversation.support_messages.build(user_id: current_user.id)
       message.body = data["body"].present? ? data.fetch("body") : nil
       if message.save
         data = {}
@@ -25,8 +23,7 @@ class ConversationChannel < ApplicationCable::Channel
         data["updated_at"] = message.updated_at
         ActionCable.server.broadcast "conversations_#{message.conversation_id}", data.as_json
       end
+    
     end
   end
-
 end
-

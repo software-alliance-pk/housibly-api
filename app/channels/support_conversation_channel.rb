cd04efff68279706c57600e37de7441b2859fba3
vs-ror <backend@viralsquare.org>
2022-09-02 20:32:38 +0900

    supportcoversation channel

--- app/channels/support_conversation_channel.rb
+++ app/channels/support_conversation_channel.rb
@@ -1,8 +1,10 @@
 class SupportConversationChannel < ApplicationCable::Channel
   def subscribed
- stop_all_streams
+    stop_all_streams
     SupportConversation.where(sender_id: current_user).or(SupportConversation.where(recipient_id: current_user)).find_each do |conversation|
-      stream_from "conversations_#{conversation.id}"  end
+      stream_from "conversations_#{conversation.id}" 
+   end
+ end
 
   def unsubscribed
     stop_all_streams

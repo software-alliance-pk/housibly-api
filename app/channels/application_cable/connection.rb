44f5349ce473aeb4dd70251db4a67e6507bf7ce0
vs-ror <backend@viralsquare.org>
2022-09-02 16:49:07 +0900

    puts file

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,6 +4,7 @@ module ApplicationCable
     def connect
       begin
       @jwt_token = request.params[:token]
+      puts "<<<<<<<<<<<<current_user<<<<<<<<<<<<<<"
       self.current_user = find_verified_user
       rescue
         puts "connection is not created"
@@ -14,9 +15,11 @@ module ApplicationCable
     attr_reader :jwt_token
     def find_verified_user
       payload = decode_token
+      puts "<<<<<<<<<payload<<<<<<<<<<<<"
       User.find_by_email(payload["email"])
     end
     def decode_token
+      puts "<<<<<<jwt_token<<<<<<<<<<<"
       JsonWebTokenService.decode(jwt_token.to_s)
     end
   end

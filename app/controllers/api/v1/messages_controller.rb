dd93707fd34f67956634813743a6af311fa5444c
vs-ror <backend@viralsquare.org>
2022-09-13 01:55:10 +0900

    response change

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -40,17 +40,17 @@ class Api::V1::MessagesController < Api::V1::ApiController
   	@notifications = []
   	conversations = Conversation.where("recipient_id = (?) OR  sender_id = (?)", @current_user.id, @current_user.id)
   	conversations.each do |conversation|
-  		# if UserNotification.where(recipient_id: @current_user.id, actor_id: conversation.sender).present?
-  	 #   notification = UserNotification.where(recipient_id: @current_user.id, actor_id: conversation.sender_id).last
-  	 #  elsif UserNotification.where(recipient_id: @current_user.id, actor_id: conversation.recipient_id).present?
-  	 #   notification = UserNotification.where(recipient_id: @current_user.id, actor_id: conversation.recipient_id).last
-  	 #  end
-
-  	  if UserNotification.where(recipient_id: conversation.recipient_id, actor_id: conversation.sender_id).present?
-  	   notification = UserNotification.where(recipient_id: conversation.recipient_id, actor_id: conversation.sender_id).last
-  	  elsif UserNotification.where(recipient_id: conversation.sender_id, actor_id: conversation.recipient_id).present?
-  	   notification = UserNotification.where(recipient_id: conversation.sender_id, actor_id: conversation.recipient_id).last
+  		if UserNotification.where(recipient_id: @current_user.id, actor_id: conversation.sender).present?
+  	   notification = UserNotification.where(recipient_id: @current_user.id, actor_id: conversation.sender_id).last
+  	  elsif UserNotification.where(recipient_id: @current_user.id, actor_id: conversation.recipient_id).present?
+  	   notification = UserNotification.where(recipient_id: @current_user.id, actor_id: conversation.recipient_id).last
   	  end
+
+  	  # if UserNotification.where(recipient_id: conversation.recipient_id, actor_id: conversation.sender_id).present?
+  	  #  notification = UserNotification.where(recipient_id: conversation.recipient_id, actor_id: conversation.sender_id).last
+  	  # elsif UserNotification.where(recipient_id: conversation.sender_id, actor_id: conversation.recipient_id).present?
+  	  #  notification = UserNotification.where(recipient_id: conversation.sender_id, actor_id: conversation.recipient_id).last
+  	  # end
   	   @notifications << notification
   	end
 	if @notifications

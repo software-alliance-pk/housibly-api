5b6f9d80af69056860e61255bef7ca8d6046ab4e
vs-ror <backend@viralsquare.org>
2022-09-20 00:39:42 +0900

    stripe payment

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -1,6 +1,8 @@
 class StripeService
   require 'stripe'
-  Stripe.api_key = Rails.application.credentials.stripe[:api_key] if Rails.env.development?
+  # Stripe.api_key = Rails.application.credentials.stripe[:api_key] if Rails.env.development?
+    Stripe.api_key ="sk_test_51Lf25xJxAUizx0q5nlLODfQpgzjCZox9nBzMEGUc3hzSW4ywx7GOU69fuA0FyJ30GSyhIkGFX1RadDP4NuAyqc8B00xyKRAs2h"
+
   Stripe.api_key = 'sk_test_51LNZ3BAsady3KIaWsrai2Zq9cT9PCOp5s8AF6JjSyutqxodm7ESoI8EFCKtfC5Cd79CxcklRNVD76aOBwP8XnpO400X2CvQDdP' if Rails.env.production?
 
   def self.create_customer(name, email)
@@ -10,6 +12,33 @@ class StripeService
         email: email
       })
   end
+
+  def self.create_subscription(customer_id,price_id)
+    debugger
+    Stripe::Subscription.create({
+  customer: customer_id,
+  items: [
+    {price: price_id},
+  ],
+})
+  end
+
+  def self.create_product
+   product=  Stripe::Product.create(
+      {
+        name: '1 YEAR'
+      }
+      )
+   Stripe::Price.create({
+  unit_amount: 8000,
+  currency: 'usd',
+  recurring: {interval: 'month'},
+  product: product.id,
+})
+
+
+  end
+  
 
   def self.create_card(customer_id, token)
     Stripe::Customer.create_source(

1c12a1fbf9c36800b9536cd9da2ba35b9888b7d5
vs-ror <backend@viralsquare.org>
2022-09-22 01:06:12 +0900

    fixed

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -34,9 +34,10 @@ def index
       data["avatar"] = conversation&.recipient&.avatar&.url
     else
       data["avatar"] = conversation&.sender&.avatar&.url
+
     end
-      ActionCable.server.broadcast "user_chat_list_#{conversation.id}",  { data:  data.as_json}
-      # ActionCable.server.broadcast "user_chat_list_#{conversation&.sender_id}",  { data:  data.as_json}
+      ActionCable.server.broadcast "user_chat_list_#{conversation&.recipient_id}",  { data:  data.as_json}
+      ActionCable.server.broadcast "user_chat_list_#{conversation&.sender_id}",  { data:  data.as_json}
     end
 end
 def read_messages

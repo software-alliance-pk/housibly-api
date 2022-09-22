d8edda638dcbc4c613314871086ed36792a454bf
ror-vs <backend@viralsquare.org>
2022-09-22 20:09:37 +0500

    COde commented

--- app/jobs/subscription_created_job.rb
+++ app/jobs/subscription_created_job.rb
@@ -1,10 +1,5 @@
-class SubscriptionCreatedJob< ApplicationRecord
-	queue_as :default
-  def self.perform_now(*args)
-   debugger
-  end
-
-  def parse_data(payload)
-
-  end
-end
+# class SubscriptionCreatedJob< ApplicationRecord
+#   queue_as :default
+#   def self.perform_now(*args)
+#   end
+# end

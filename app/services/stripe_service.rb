c85f8b12f760057f59d6d948457a8c622e76f850
ror-vs <backend@viralsquare.org>
2022-09-23 14:10:11 +0500

    Stripe key updated

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -1,9 +1,8 @@
 class StripeService
   require 'stripe'
   # Stripe.api_key = Rails.application.credentials.stripe[:api_key] if Rails.env.development?
-    Stripe.api_key ="sk_test_51Lf25xJxAUizx0q5nlLODfQpgzjCZox9nBzMEGUc3hzSW4ywx7GOU69fuA0FyJ30GSyhIkGFX1RadDP4NuAyqc8B00xyKRAs2h"
-
-  Stripe.api_key = 'sk_test_51LNZ3BAsady3KIaWsrai2Zq9cT9PCOp5s8AF6JjSyutqxodm7ESoI8EFCKtfC5Cd79CxcklRNVD76aOBwP8XnpO400X2CvQDdP' if Rails.env.production?
+  Stripe.api_key ="sk_test_51Lf25xJxAUizx0q5nlLODfQpgzjCZox9nBzMEGUc3hzSW4ywx7GOU69fuA0FyJ30GSyhIkGFX1RadDP4NuAyqc8B00xyKRAs2h"
+  Stripe.api_key = 'sk_test_51Lf25xJxAUizx0q5nlLODfQpgzjCZox9nBzMEGUc3hzSW4ywx7GOU69fuA0FyJ30GSyhIkGFX1RadDP4NuAyqc8B00xyKRAs2h' if Rails.env.production?
 
   def self.create_customer(name, email)
     Stripe::Customer.create(

8bb10519adc735466d00391040e76d309df0d5dd
ror-vs <backend@viralsquare.org>
2022-09-27 01:55:47 +0500

    Handle edge cases in dream addresses

--- app/controllers/api/v1/dream_addresses_controller.rb
+++ app/controllers/api/v1/dream_addresses_controller.rb
@@ -1,32 +1,36 @@
 class Api::V1::DreamAddressesController < Api::V1::ApiController
   def create
-    @address = @current_user.dream_addresses.build(location: params[:location])
-    if @address.save
-      @address
+    if params[:location].present?
+      @address = @current_user.dream_addresses.build(location: params[:location])
+      if @address.save
+        @address
+      else
+        render_error_messages(@address)
+      end
     else
-      render_error_messages(@address)
+      render json: { message: "Location parameter is missing" }, status: :ok
     end
   end
 
   def fetch_address
     addresses = []
-      array = eval(params[:polygon])
-      array.each do |address|
+    array = eval(params[:polygon])
+    array.each do |address|
       lat = address[:latitude]
       long = address[:longitude]
       address = Geocoder.search([lat, long])
       house_number = address.first.house_number
       city = address.first.city
       country = address.first.country
-      address = Property.where("address ILIKE ? AND address ILIKE ? AND address ILIKE ?", "%#{house_number}%","%#{city}%","%#{country}%")
+      address = Property.where("address ILIKE ? AND address ILIKE ? AND address ILIKE ?", "%#{house_number}%", "%#{city}%", "%#{country}%")
       unless address == nil
         addresses << address
       end
     end
     if addresses.present?
-      render json: {message: addresses}, status: :ok
+      render json: { message: addresses }, status: :ok
     else
-      render json: {message: "Dream Address Not Foud"}, status: :ok
+      render json: { message: addresses }, status: :ok
     end
   end
 
@@ -34,32 +38,33 @@ class Api::V1::DreamAddressesController < Api::V1::ApiController
     property = Property.find_by(zip_code: params[:zip_code])
     if property.present?
       address = Geocoder.search("params[:zip_code]")
-      render json: {message: address},status: :ok
+      render json: { message: address }, status: :ok
     else
-      render json: {message: "No Address available"}
+      render json: { message: "No Address available" }
     end
   end
-
 
   def index
     @dream_addresses = @current_user.dream_addresses
-    if @dream_addresses
+    if @dream_addresses.present?
       @dream_addresses
     else
-      render json: {message: "Dream Address Not Foud"}, status: :ok
+      render json: { message: @dream_address }, status: :ok
     end
   end
+
   def destroy
-    @dream_address = DreamAddress.find_by(id: params[:id])
-    if @dream_address.present?
-      if @dream_address.destroy
-        render json: {message: "successfully deleted"}, status: :ok
-     end
-   else
-    render json: {message: "Dream Address Not Foud"}, status: :ok
+    if params[:id].present?
+      @dream_address = DreamAddress.find_by(id: params[:id])
+      if @dream_address.present?
+         @dream_address.destroy
+         render json: { message: "successfully deleted" }, status: :ok
+      else
+        render json: { message: @dream_address }, status: :ok
+      end
+    else
+      render json: { message: "Dream address id is missing" }, status: :ok
     end
   end
-
-
 
 end

608ccf6fbcb5392cc96a257f0f421670788dda1f
ror-vs <backend@viralsquare.org>
2022-09-26 14:19:23 +0500

    Comment Duplicate Column

--- db/migrate/20220926082443_add_more_columns_to_subscription.rb
+++ db/migrate/20220926082443_add_more_columns_to_subscription.rb
@@ -2,7 +2,7 @@ class AddMoreColumnsToSubscription < ActiveRecord::Migration[6.1]
   def change
     change_column :subscriptions, :current_period_start, :string
     change_column :subscriptions, :current_period_end, :string
-    add_column :subscriptions, :payment_currency, :string
-    add_column :subscriptions, :payment_nature, :string
+    #add_column :subscriptions, :payment_currency, :string
+    #add_column :subscriptions, :payment_nature, :string
   end
 end

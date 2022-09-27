2b60538db884d833b27fd1573ef6d65a06683d48
ror-vs <backend@viralsquare.org>
2022-09-27 12:21:25 +0500

    Api

--- app/controllers/api/v1/dream_addresses_controller.rb
+++ app/controllers/api/v1/dream_addresses_controller.rb
@@ -13,9 +13,10 @@ class Api::V1::DreamAddressesController < Api::V1::ApiController
   end
 
   def fetch_address
+    weight_age = 0
     addresses = []
-    property_list = ''
-    if params[:polygon].present?
+    @property_list = ''
+    if params[:using_polygon] == true
       array = eval(params[:polygon])
       array.each do |address|
         lat = address[:latitude]
@@ -34,29 +35,43 @@ class Api::V1::DreamAddressesController < Api::V1::ApiController
       else
         render json: { message: addresses }, status: :ok
       end
-    elsif params[:zip_code].present?
+    elsif params[:using_zip_code] == true
 
-    elsif params[:user_preference].present?
-      property_list = Property.search_property_by_total_number_of_rooms(params[:number_of_rooms])
-      property_list = Property.search_property_by_total_parking_spaces(params[:number_of_parking_spaces])
-      property_list = Property.search_property_by_bed_rooms(params[:bed_rooms])
-      property_list = Property.search_property_by_title(params[:title])
-      property_list = Property.search_property_by_house_style(params[:house_style])
-      property_list = Property.search_property_by_house_type(params[:house_type])
-      property_list = Property.search_property_by_air_conditioner(params[:air_conditioner])
-      property_list = Property.search_property_by_price(params[:price])
-      property_list = Property.search_property_by_condo_type(params[:condo_type])
-      property_list = Property.search_property_by_condo_style(params[:condo_style])
-      property_list = Property.search_property_by_lot_frontage_unit(params[:frontage_unit])
-      property_list = Property.search_property_by_lot_depth_unit(params[:depth_unit])
-      property_list = Property.search_property_by_garage_spaces(params[:garage_spaces])
+    elsif params[:user_preference] == true
+      property_list_having_rooms = Property.search_property_by_total_number_of_rooms(params[:number_of_rooms])
+      weight_age = calculate_weightage(weight_age,property_list_having_rooms,14)
+      property_list_having_parking_space = Property.search_property_by_total_parking_spaces(params[:number_of_parking_spaces])
+      weight_age = calculate_weightage(weight_age,property_list_having_parking_space,14)
+      property_list_having_bed_rooms = Property.search_property_by_bed_rooms(params[:bed_rooms])
+      property_list_having_title = Property.search_property_by_title(params[:title])
+      property_list_having_style = Property.search_property_by_house_style(params[:house_style]) ||
+        Property.search_property_by_condo_style(params[:condo_style])
+      property_list_having_type = Property.search_property_by_house_type(params[:house_type]) ||
+        Property.search_property_by_condo_type(params[:condo_type])
+      property_list_having_air_conditioner = Property.search_property_by_air_conditioner(params[:air_conditioner])
+      property_list_having_price = Property.search_property_by_price(params[:price])
+      property_list_having_frontage_unit = Property.search_property_by_lot_frontage_unit(params[:frontage_unit])
+      property_list_having_depth_unit = Property.search_property_by_lot_depth_unit(params[:depth_unit])
+      property_list_having_garage_spaces = Property.search_property_by_garage_spaces(params[:garage_spaces])
+      @property_list = (property_list_having_rooms + property_list_having_air_conditioner+
+        property_list_having_bed_rooms+property_list_having_depth_unit+ property_list_having_garage_spaces+
+        property_list_having_style+property_list_having_parking_space+ property_list_having_price+
+        property_list_having_frontage_unit+
+        property_list_having_type+
+        property_list_having_title)&.uniq
+
     end
+  end
+
+
+  def calculate_weightage(weight_age,matching_item,number)
+    weight_age= weight_age + number if matching_item.present?
   end
 
   def fetch_by_zip_code
     property = Property.find_by(zip_code: params[:zip_code])
     if property.present?
-      address = Geocoder.search("params[:zip_code]")
+      address = Geocoder.search(params[:zip_code])
       render json: { message: address }, status: :ok
     else
       render json: { message: "No Address available" }

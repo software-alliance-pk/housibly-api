11a4d43f01b5ea1c2118e579ac225970cf2de88a
ror-vs <backend@viralsquare.org>
2022-09-30 04:03:39 +0500

    Remove Geocoded Functionality from Dream Addresses

--- app/models/dream_address.rb
+++ app/models/dream_address.rb
@@ -1,9 +1,7 @@
 class DreamAddress < ApplicationRecord
+  before_commit :add_the_lnt_and_lng_property
   include CsvCounter
   belongs_to :user
-  geocoded_by :location
-  after_validation :geocode, :if => :location_changed?
-
   def self.to_csv
     CsvCounter.update_csv_counter
     CSV.generate(headers: true) do |csv|
@@ -12,5 +10,14 @@ class DreamAddress < ApplicationRecord
         csv << record.attributes.values_at(*attribute_names)
       end
     end
+  end
+
+  def add_the_lnt_and_lng_property
+    location  = LocationFinderService.get_location_attributes(self.address)
+    self.longitude = location[:long]
+    self.latitude = location[:lat]
+    # location[:country]
+    # location[:city]
+    # location[:district]
   end
 end

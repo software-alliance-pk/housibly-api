394ec6fbaec66b3568db47ed4e0c5ed9098bdf16
vs-ror <backend@viralsquare.org>
2022-09-29 20:42:01 +0900

    fixed

--- app/controllers/api/v1/dream_addresses_controller.rb
+++ app/controllers/api/v1/dream_addresses_controller.rb
@@ -17,21 +17,29 @@ class Api::V1::DreamAddressesController < Api::V1::ApiController
     @property = []
     @property_list = ''
     if params[:using_polygon] == "true"
-      array = eval(params[:polygon])
-      array.each do |address|
-        lat = address[:latitude]
-        long = address[:longitude]
-        address = Geocoder.search([lat, long])
-        house_number = address.first.house_number
-        city = address.first.city
-        country = address.first.country
-        property = Property.find_by("address ILIKE ? AND address ILIKE ? AND address ILIKE ?", "%#{house_number}%", "%#{city}%", "%#{country}%")
-        unless property == nil
-          property.weight_age = "100"
-          @property << property
+      if params[:polygon].present?
+        array = eval(params[:polygon])
+        array.each do |address|
+          lat = address[:latitude]
+          long = address[:longitude]
+          address = Geocoder.search([lat, long])
+          house_number = address.first.house_number
+          city = address.first.city
+          country = address.first.country
+          property = Property.find_by("address ILIKE ? AND address ILIKE ? AND address ILIKE ?", "%#{house_number}%", "%#{city}%", "%#{country}%")
+          unless property == nil
+            property.weight_age = "100"
+            @property << property
+          end
         end
+          if @property.present?
+            @property
+          else
+            render json: {message: "Any property does not match"},status: :unprocessable_entity
+          end
+      else
+        render json: {message: "Please give suitable parameter"},status: :unprocessable_entity
       end
-      @property
     elsif params[:using_zip_code] == "true"
       @property = []
       @property_list = Property.where(zip_code: params[:zip_code])
@@ -39,173 +47,176 @@ class Api::V1::DreamAddressesController < Api::V1::ApiController
         record.weight_age = "100"
         @property << record
       end
-      @property
-    elsif params[:user_preference] == "true"
-      if @current_user.user_preference.present?
-        property_list_having_bed_rooms = (Property.ransack(min_bed_rooms_lteq_any: @current_user.user_preference.min_bedrooms).result || Property.ransack(max_bed_rooms_gteq_any: @current_user.user_preference.max_bathrooms).result)&.uniq
-        _value = calculate_weightage(_weight_age,property_list_having_bed_rooms,14)
-        _weight_age = _value if _value.present?
-        property_list_having_style = Property.search_property_by_house_style(@current_user.user_preference.property_style) ||
-        Property.search_property_by_condo_style(@current_user.user_preference.property_style)
-        _value = calculate_weightage(_weight_age,property_list_having_style,14)
-        _weight_age = _value if _value.present?
-        property_list_having_type = Property.search_property_by_house_type(@current_user.user_preference.property_type) ||
-        Property.search_property_by_condo_type(@current_user.user_preference.property_type)
-        _value = calculate_weightage(_weight_age,property_list_having_type,14)
-        _weight_age = _value if _value.present?
-        _weight_age = _value if _value.present?
-        property_list_having_price = (Property.ransack(price_lteq_any: @current_user.user_preference.min_price).result  || Property.ransack(price_gteq_any: @current_user.user_preference.max_price).result).uniq
-        _value = calculate_weightage(_weight_age,property_list_having_price,14)
-        _weight_age = _value if _value.present?
-        property_list_having_frontage_unit = Property.search_property_by_lot_frontage_unit(@current_user.user_preference.min_lot_frontage)
-        _value = calculate_weightage(_weight_age,property_list_having_frontage_unit,14)
-        # propert_having_dream_address= Property.where(address: @current_user.dream_addresses.pluck(:location))
-        # _weight_age = "100" if propert_having_dream_address.present?
-        # debugger
-        # unless propert_having_dream_address.present?
-          _weight_age = _value if _value.present?
-          @property_list = (property_list_having_bed_rooms+
-          property_list_having_style+ property_list_having_price+
-          property_list_having_frontage_unit+
-          property_list_having_type)&.uniq
-          @property_list.each do |record|
-            record.weight_age = _weight_age
-            @property << record
-          end
-        # else
-        #     @property_list = propert_having_dream_address
-        #     @property_list.each do |record|
-        #       record.weight_age = _weight_age
-        #       @property << record
-        #     end
-        #end
-        else
-         @property
-       end
+      if @property.present?
+        @property
+      else
+        render json: {message: "Any property does not match"},status: :unprocessable_entity
+      end
+  elsif params[:user_preference] == "true"
+    if @current_user.user_preference.present?
+      property_list_having_bed_rooms = (Property.ransack(min_bed_rooms_lteq_any: @current_user.user_preference.min_bedrooms).result || Property.ransack(max_bed_rooms_gteq_any: @current_user.user_preference.max_bathrooms).result)&.uniq
+      _value = calculate_weightage(_weight_age,property_list_having_bed_rooms,14)
+      _weight_age = _value if _value.present?
+      property_list_having_style = Property.search_property_by_house_style(@current_user.user_preference.property_style) ||
+      Property.search_property_by_condo_style(@current_user.user_preference.property_style)
+      _value = calculate_weightage(_weight_age,property_list_having_style,14)
+      _weight_age = _value if _value.present?
+      property_list_having_type = Property.search_property_by_house_type(@current_user.user_preference.property_type) ||
+      Property.search_property_by_condo_type(@current_user.user_preference.property_type)
+      _value = calculate_weightage(_weight_age,property_list_having_type,14)
+      _weight_age = _value if _value.present?
+      _weight_age = _value if _value.present?
+      property_list_having_price = (Property.ransack(price_lteq_any: @current_user.user_preference.min_price).result  || Property.ransack(price_gteq_any: @current_user.user_preference.max_price).result).uniq
+      _value = calculate_weightage(_weight_age,property_list_having_price,14)
+      _weight_age = _value if _value.present?
+      property_list_having_frontage_unit = Property.search_property_by_lot_frontage_unit(@current_user.user_preference.min_lot_frontage)
+      _value = calculate_weightage(_weight_age,property_list_having_frontage_unit,14)
+      _weight_age = _value if _value.present?
+      @property_list = (property_list_having_bed_rooms+
+        property_list_having_style+ property_list_having_price+
+        property_list_having_frontage_unit+
+        property_list_having_type)&.uniq
+      @property_list.each do |record|
+        record.weight_age = _weight_age
+        @property << record
+      end
+      if @property.present?
+        @property
+      else
+       render json: {message: "Any property does not match"},status: :unprocessable_entity
+      end
+   else
+     render json: {message: "Any property does not match"},status: :unprocessable_entity
+   end
+ # end
 
-     else
-      render json: {message: "Please give user preference, zip code or polygon"},status: :ok
-    end
-  end
-  def user_pereference_match
+else
+  render json: {message: "Please give user preference, zip code or polygon"},status: :unprocessable_entity
+end
+
+
+def fetch_user
+  if params[:user_preference] == "true"
     _weight_age = 0
     @property = []
     @property_list = ''
     property = Property.find_by(id: params[:property_id])
-    property_list_having_bed_rooms = UserPreference.ransack(min_bed_rooms_lteq_any: property.bed_rooms).result
-    property_list_having_bed_rooms = UserPreference.ransack(max_bed_rooms_gteq_any: property.bed_rooms).result
-    _value = calculate_weightage(_weight_age,property_list_having_bed_rooms,14)
+    user_preference_list_having_bed_rooms = UserPreference.ransack(min_bed_rooms_lteq_any: property.bed_rooms).result
+    user_preference_list_having_bed_rooms = UserPreference.ransack(max_bed_rooms_gteq_any: property.bed_rooms).result
+    _value = calculate_weightage(_weight_age,user_preference_list_having_bed_rooms,14)
     _weight_age = _value if _value.present?
-    property_list_having_style = UserPreference.ransack(property_style_matches: "%#{property.house_style}").result ||
+    user_preference_list_having_style = UserPreference.ransack(property_style_matches: "%#{property.house_style}").result ||
     UserPreference.ransack(property_style_matches: "%#{property.condo_style}").result
-    _value = calculate_weightage(_weight_age,property_list_having_style,14)
+    _value = calculate_weightage(_weight_age,user_preference_list_having_style,14)
     _weight_age = _value if _value.present?
-    property_list_having_type = UserPreference.ransack(property_type_matches: "%#{property.house_type}").result ||
+    user_preference_list_having_type = UserPreference.ransack(property_type_matches: "%#{property.house_type}").result ||
     UserPreference.ransack(property_style_matches: "%#{property.condo_type}").result
-    _value = calculate_weightage(_weight_age,property_list_having_type,14)
+    _value = calculate_weightage(_weight_age,user_preference_list_having_type,14)
     _weight_age = _value if _value.present?
         # _weight_age = _value if _value.present?
-        property_list_having_price = UserPreference.ransack(min_price_lteq_any: property.price).result
-        property_list_having_price = UserPreference.ransack(max_price_gteq_any: property.price).result
-        _value = calculate_weightage(_weight_age,property_list_having_price,14)
+        user_preference_list_having_price = UserPreference.ransack(min_price_lteq_any: property.price).result
+        user_preference_list_having_price = UserPreference.ransack(max_price_gteq_any: property.price).result
+        _value = calculate_weightage(_weight_age,user_preference_list_having_price,14)
         _weight_age = _value if _value.present?
-        property_list_having_frontage_unit = UserPreference.ransack(min_lot_frontage_match: property.lot_frontage).result
-        _value = calculate_weightage(_weight_age,property_list_having_frontage_unit,14)
+        user_preference_list_having_frontage_unit = UserPreference.ransack(min_lot_frontage_match: property.lot_frontage).result
+        _value = calculate_weightage(_weight_age,user_preference_list_having_frontage_unit,14)
         _weight_age = _value if _value.present?
-        @property_list = (property_list_having_bed_rooms+
-          property_list_having_style+ property_list_having_price+
-          property_list_having_frontage_unit+
-          property_list_having_type)&.uniq
-        @property_list.each do |record|
+        @user_prefernce_list = (user_preference_list_having_bed_rooms+
+          user_preference_list_having_style+ user_preference_list_having_price+
+          user_preference_list_having_frontage_unit+
+          user_preference_list_having_type)&.uniq
+        @user_prefernce_list.each do |record|
           record.weight_age = _weight_age
-          @property << record
+          @user_prefernce << record
         end
-        if @property
-          @property
+        if @user_prefernce.present?
+          @user_prefernce
         else
           render json: "no match"
         end
-      end
-
-      def newest_first
-        _weight_age = 0
-        @property = []
-        @property_list = ''
-        property = Property.find_by(id: params[:property_id])
-        property_list_having_bed_rooms = UserPreference.ransack(min_bed_rooms_lteq_any: property.bed_rooms).result
-        property_list_having_bed_rooms = UserPreference.ransack(max_bed_rooms_gteq_any: property.bed_rooms).result
-        _value = calculate_weightage(_weight_age,property_list_having_bed_rooms,14)
+      elsif params[:newest_first] == "true"
+        # property = Property.find_by(id: params[:property_id])
+        user_preference = UserPreference.joins(:user).where("users.created_at = ?", 1.week.ago)
+        user_preference_list_having_bed_rooms = user_preference.ransack(min_bed_rooms_lteq_any: property&.bed_rooms).result
+        user_preference_list_having_bed_rooms = user_preference.ransack(max_bed_rooms_gteq_any: property&.bed_rooms).result
+        _value = calculate_weightage(_weight_age,user_preference_list_having_bed_rooms,14)
         _weight_age = _value if _value.present?
-        property_list_having_style = UserPreference.ransack(property_style_matches: "%#{property.house_style}").result ||
-        UserPreference.ransack(property_style_matches: "%#{property.condo_style}").result
-        _value = calculate_weightage(_weight_age,property_list_having_style,14)
+        user_preference_list_having_style = user_preference.ransack(property_style_matches: "%#{property&.house_style}").result ||
+        user_preference.ransack(property_style_matches: "%#{property.condo_style}").result
+        _value = calculate_weightage(_weight_age,user_preference_list_having_style,14)
         _weight_age = _value if _value.present?
-        property_list_having_type = UserPreference.ransack(property_type_matches: "%#{property.house_type}").result ||
-        UserPreference.ransack(property_style_matches: "%#{property.condo_type}").result
-        _value = calculate_weightage(_weight_age,property_list_having_type,14)
+        user_preference_list_having_type = user_preference.ransack(property_type_matches: "%#{property&.house_type}").result ||
+        user_preference.ransack(property_style_matches: "%#{property&.condo_type}").result
+        _value = calculate_weightage(_weight_age,user_preference_list_having_type,14)
         _weight_age = _value if _value.present?
         # _weight_age = _value if _value.present?
-        property_list_having_price = UserPreference.ransack(min_price_lteq_any: property.price).result
-        property_list_having_price = UserPreference.ransack(max_price_gteq_any: property.price).result
-        _value = calculate_weightage(_weight_age,property_list_having_price,14)
+        user_preference_list_having_price = user_preference.ransack(min_price_lteq_any: property&.price).result
+        user_preference_list_having_price = user_preference.ransack(max_price_gteq_any: property&.price).result
+        _value = calculate_weightage(_weight_age,user_preference_list_having_price,14)
         _weight_age = _value if _value.present?
-        property_list_having_frontage_unit = UserPreference.ransack(min_lot_frontage_match: property.lot_frontage).result
-        _value = calculate_weightage(_weight_age,property_list_having_frontage_unit,14)
+        user_preference_list_having_frontage_unit = user_preference.ransack(min_lot_frontage_match: property&.lot_frontage).result
+        _value = calculate_weightage(_weight_age,user_preference_list_having_frontage_unit,14)
         _weight_age = _value if _value.present?
-        @property_list = (property_list_having_bed_rooms+
-          property_list_having_style+ property_list_having_price+
-          property_list_having_frontage_unit+
-          property_list_having_type)&.uniq
-        @property_list.each do |record|
+        @user_prefernce_list = (user_preference_list_having_bed_rooms+
+          user_preference_list_having_style+ user_preference_list_having_price+
+          user_preference_list_having_frontage_unit+
+          user_preference_list_having_type)&.uniq
+        @user_prefernce_list.each do |record|
           record.weight_age = _weight_age
-      #debugger
-      #record =  record.join(:user).where("users.created_at = ?",1.week.ago)
-      @property << record
-    end
+          @user_prefernce << record
+        end
     #@property = @property.join(:user).where("users.created_at = ?",1.week.ago)
-    if @property
-      @property
+    if @user_prefernce
+      @user_prefernce_list
     else
       render json: "no match"
     end
+  elsif params[:draw_map] == "true"
+  elsif params[:dream_addresses] == "true"
+  elsif params[:top_match] == "true"
+
   end
+end
 
 
-  def calculate_weightage(_weight_age,matching_item,number)
-    _weight_age = _weight_age + number if matching_item.present?
+end
+
+
+def calculate_weightage(_weight_age,matching_item,number)
+  _weight_age = _weight_age + number if matching_item.present?
+end
+
+def fetch_by_zip_code
+  property = Property.find_by(zip_code: params[:zip_code])
+  if property.present?
+    address = Geocoder.search(params[:zip_code])
+    render json: { message: address }, status: :ok
+  else
+    render json: { message: "No Address available" }
   end
+end
 
-  def fetch_by_zip_code
-    property = Property.find_by(zip_code: params[:zip_code])
-    if property.present?
-      address = Geocoder.search(params[:zip_code])
-      render json: { message: address }, status: :ok
-    else
-      render json: { message: "No Address available" }
-    end
+def index
+  @dream_addresses = @current_user.dream_addresses
+  if @dream_addresses.present?
+    @dream_addresses
+  else
+    render json: { message: @dream_address }, status: :ok
   end
+end
 
-  def index
-    @dream_addresses = @current_user.dream_addresses
-    if @dream_addresses.present?
-      @dream_addresses
+def destroy
+  if params[:id].present?
+    @dream_address = DreamAddress.find_by(id: params[:id])
+    if @dream_address.present?
+      @dream_address.destroy
+      render json: { message: "successfully deleted" }, status: :ok
     else
       render json: { message: @dream_address }, status: :ok
     end
+  else
+    render json: { message: "Dream address id is missing" }, status: :ok
   end
-
-  def destroy
-    if params[:id].present?
-      @dream_address = DreamAddress.find_by(id: params[:id])
-      if @dream_address.present?
-        @dream_address.destroy
-        render json: { message: "successfully deleted" }, status: :ok
-      else
-        render json: { message: @dream_address }, status: :ok
-      end
-    else
-      render json: { message: "Dream address id is missing" }, status: :ok
-    end
-  end
+end
 
 end

dbe2f47ca2c0b7435127b577b0122bc99d46dc18
ror-vs <backend@viralsquare.org>
2022-09-29 17:36:38 +0500

    Fixed the notification issue

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -135,26 +135,26 @@ class Api::V1::UsersController < Api::V1::ApiController
   def update_notification
     if params[:push_notification].present?
       if params[:push_notification] == "false"
-        @current_user.user_setting.update(push_notification: params[:push_notification])
+        @current_user.user_setting.update(push_notification: false)
         render json: { message: "Push Notification OFF" }, status: :ok
       else
-        @current_user.user_setting.update(push_notification: params[:push_notification])
+        @current_user.user_setting.update(push_notification: true)
         render json: { message: "Push Notification ON" }, status: :ok
       end
     elsif params[:inapp_notification].present?
       if params[:inapp_notification] == "false"
-        @current_user.user_setting.update(inapp_notification: params[:inapp_notification])
+        @current_user.user_setting.update(inapp_notification: false)
         render json: { message: "In App Notification OFF" }, status: :ok
       else
-        @current_user.user_setting.update(inapp_notification: params[:inapp_notification])
+        @current_user.user_setting.update(inapp_notification: true)
         render json: { message: "In App Notification ON" }, status: :ok
       end
     else
       if params[:email_notification] == "false"
-        @current_user.user_setting.update(email_notification: params[:email_notification])
+        @current_user.user_setting.update(email_notification: false)
         render json: { message: "In App Notification OFF" }, status: :ok
       else
-        @current_user.user_setting.update(email_notification: params[:email_notification])
+        @current_user.user_setting.update(email_notification: true)
         render json: { message: "In App Notification ON" }, status: :ok
       end
     end

2b131a2b2d38e670df4c17129fbebfed70e1f9ef
ror-vs <backend@viralsquare.org>
2022-09-30 14:29:33 +0500

    Fixed

--- app/services/user_current_location_service.rb
+++ app/services/user_current_location_service.rb
@@ -1,14 +1,15 @@
 class UserCurrentLocationService
   def call(location)
     res = location
+    puts res.data
     puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
     puts " SIGN UP USER"
-    puts res[:ip]
-    puts res[:ip]
-    puts res[:loc]
-    puts res[:postal]
+    puts res.data[:ip]
+    puts res.data[:ip]
+    puts res.data[:loc]
+    puts res.data[:postal]
     puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    cordinates = res[:loc]&.split(",")
+    cordinates = res.data[:loc]&.split(",")
     puts LocationFinderService.get_location_attributes_by_reverse(cordinates)
 
   end

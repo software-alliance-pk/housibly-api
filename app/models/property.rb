f04dea043fd1a272a1da18a6325df18cca00bcbf
vs-ror <backend@viralsquare.org>
2022-09-30 19:56:01 +0900

    Remove geocoder and implement new gem

--- app/models/property.rb
+++ app/models/property.rb
@@ -104,7 +104,7 @@ class Property < ApplicationRecord
 
   def add_the_lnt_and_lng_property
     location  = LocationFinderService.get_location_attributes(self.address)
-    self.update(longtitude:location[:long],latitude: location[:lat],zip_code: location[:zip_code])
+    self.update(longitude:location[:long],latitude: location[:lat],zip_code: location[:zip_code])
     # location[:country]
     # location[:city]
     # location[:district]
--- app/services/zip_code_search_service.rb
+++ app/services/zip_code_search_service.rb
@@ -3,8 +3,8 @@ class ZipCodeSearchService
     @property = []
   end
 
-  def search_property(params)
-    property = Property.where(zip_code: params[:zip_code])
+  def search_property(zip_code)
+    property = Property.where(zip_code: zip_code)
     if property.present?
       property.each do |record|
         record.weight_age = "100"

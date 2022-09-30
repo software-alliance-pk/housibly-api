4056c595f6f6445c349e6a1428a68f8cc800ae76
ror-vs <backend@viralsquare.org>
2022-09-30 14:32:45 +0500

    Add quotes

--- app/services/user_current_location_service.rb
+++ app/services/user_current_location_service.rb
@@ -4,12 +4,11 @@ class UserCurrentLocationService
     puts res.data
     puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
     puts " SIGN UP USER"
-    puts res.data[:ip]
-    puts res.data[:ip]
-    puts res.data[:loc]
-    puts res.data[:postal]
+    puts res.data["ip"]
+    puts res.data["loc"]
+    puts res.data["postal"]
     puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    cordinates = res.data[:loc]&.split(",")
+    cordinates = res.data["loc"]&.split(",")
     puts LocationFinderService.get_location_attributes_by_reverse(cordinates)
 
   end

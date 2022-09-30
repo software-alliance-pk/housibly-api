39f4dd67b7b68ff94f29f61fb58eeaec18e67df3
ror-vs <backend@viralsquare.org>
2022-09-30 15:03:46 +0500

    Remove puts

--- app/services/user_current_location_service.rb
+++ app/services/user_current_location_service.rb
@@ -1,15 +1,8 @@
 class UserCurrentLocationService
   def call(location)
     res = location
-    puts res.data
-    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    puts " SIGN UP USER"
-    puts res.data["ip"]
-    puts res.data["loc"]
-    puts res.data["postal"]
-    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
     cordinates = res.data["loc"]&.split(",")
-    puts LocationFinderService.get_location_attributes_by_reverse(cordinates)
-
+    location = LocationFinderService.get_location_attributes_by_reverse(cordinates)
+    return location
   end
 end

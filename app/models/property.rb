0a4c7d16c41230663197854bca54742bca01f5f5
ror-vs <backend@viralsquare.org>
2022-10-02 14:34:22 +0500

    Removing the ransack

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,25 @@
 class Property < ApplicationRecord
   after_commit :add_the_lnt_and_lng_property, on: :create
+  before_validation :create_balcony,:create_laundry,:create_security
   include PgSearch::Model
+
+  scope :price_matcher, ->(min_price,max_price){ where("price between (?) and (?)",min_price,max_price)}
+  scope :bath_rooms_matcher, ->(min_bath_room,max_bath_room){ where("bath_rooms between (?) and (?)",min_bath_room,max_bath_room)}
+  scope :bed_rooms_matcher, ->(min_bed_room,max_bed_room){ where("bed_rooms between (?) and (?)",min_bed_room,max_bed_room)}
+  scope :property_type_matcher,-> (type){ where("type ilike (?)",type.titleize)}
+  scope :property_parking_spot, -> (min_spot,max_spot){where("total_parking_spaces between (?) and (?)",min_spot,max_spot)}
+  scope :property_balcony, -> (balcony){ where("balcony = (?)",balcony)}
+  scope :property_laundry, -> (laundry){ where("laundry = (?)",laundry)}
+  scope :property_garage, -> (garage){where("garage = (?)",garage)}
+  scope :property_security, -> ( security){where("security = (?)",security)}
+  scope :property_min_lot_frontage, -> (min_lot_frontage){where("min_lot_frontage = (?)",min_lot_frontage)}
+  scope :property_min_lot_size, -> (min_lot_size,max_lot_size){where("lot_size = (?) or lot_size = (?)",min_lot_size,max_lot_size)}
+  scope :property_total_number_of_rooms, -> (min_rooms,max_rooms){ where("total_number_of_rooms between (?) and (?)",min_rooms,max_rooms)}
+  scope :property_style_matcher, -> (style){ where("condo_style = (?) or house_style = (?)",style,style)}
+  scope :property_type, -> (type){ where("condo_type = (?) or house_type = (?)",type,type)}
+  scope :property_living_space, -> (min_living,max_living){where("living_space between (?) and (?)",min_living,max_living)}
+  scope :property_age, ->  (age){where("year_built <= (?) and >=(?)",age,Date.today.strftime("%y").to_i)}
+
   pg_search_scope :search_property_by_total_number_of_rooms,
                   against: :total_number_of_rooms,
                   using: {
@@ -109,4 +128,17 @@ class Property < ApplicationRecord
     # location[:city]
     # location[:district]
   end
+
+  def create_balcony
+    self.balcony = self.balcony&.titleize
+  end
+
+  def create_laundry
+    self.laundry = self.laundry&.titleize
+  end
+
+  def create_security
+    self.security = self.security&.titleize
+  end
+
 end
---
+++ app/services/user_preferences_service.rb
@@ -0,0 +1,88 @@
+class UserPreferencesService
+
+  def initialize
+    _weight_age = 0
+    @property = []
+  end
+
+  def search_property(user)
+    _weight_age = 0
+    user_preference =  user.user_preference
+
+    property_type_records = Property.property_type_matcher(user_preference.property_type)
+    _value = calculate_weightage(_weight_age, property_type_records, 9)
+    _weight_age = _value if _value.present?
+
+    if property_type_records.present?
+      price_records  = Property.price_matcher(user_preference.min_price,user_preference.max_price)
+      _value = calculate_weightage(_weight_age, price_records, 7)
+      _weight_age = _value if _value.present?
+
+      bath_rooms_records = Property.bath_rooms_matcher(user_preference.min_bathrooms,user_preference.max_bathrooms)
+      _value = calculate_weightage(_weight_age, bath_rooms_records, 7)
+      _weight_age = _value if _value.present?
+
+
+      bed_rooms_records = Property.bed_rooms_matcher(user_preference.min_bedrooms,user_preference.max_bedrooms)
+      _value = calculate_weightage(_weight_age, bed_rooms_records, 7)
+      _weight_age = _value if _value.present?
+
+      parking_spot_records = Property.property_parking_spot(user_preference.parking_spot,user_preference.parking_spot)
+      _value = calculate_weightage(_weight_age, parking_spot_records, 7)
+      _weight_age = _value if _value.present?
+
+      garbage_spot_records = Property.property_garage(user_preference.garbage_spot)
+      _value = calculate_weightage(_weight_age,garbage_spot_records,7)
+      _weight_age = _value if _value.present?
+
+
+      security_records = Property.property_security(user_preference.security)
+      _value = calculate_weightage(_weight_age,security_records,7)
+      _weight_age = _value if _value.present?
+
+
+      min_lot_frontage_records = Property.property_min_lot_frontage(user_preference.min_lot_frontage)
+      _value = calculate_weightage(_weight_age,min_lot_frontage_records,7)
+      _weight_age = _value if _value.present?
+
+      min_lot_size_records = Property.property_min_lot_size(user_preference.min_lot_size,user_preference.max_lot_size)
+      _value = calculate_weightage(_weight_age,min_lot_size_records,7)
+      _weight_age = _value if _value.present?
+
+
+      living_space_records = Property.property_living_space(user_preference.min_living_space,user_preference.max_living_space)
+      _value = calculate_weightage(_weight_age,living_space_records,7)
+      _weight_age = _value if _value.present?
+
+
+      style_records = Property.property_style_matcher(user_preference.property_style)
+      _value = calculate_weightage(_weight_age,style_records,7)
+      _weight_age = _value if _value.present?
+
+      number_records = Property.property_total_number_of_rooms(user_preference.max_living_space)
+      _value = calculate_weightage(_weight_age,number_records ,7)
+      _weight_age = _value if _value.present?
+
+      age_records = Property.property_age(user_preference.max_age)
+      _value = calculate_weightage(_weight_age,age_records,7)
+      _weight_age = _value if _value.present?
+
+      type_records = Property.property_type(user_preference.type)
+      _value = calculate_weightage(_weight_age,type_records,7)
+      _weight_age = _value if _value.present?
+
+      @property_list = (property_type_records  + price_records + bed_rooms_records + type_records +
+        parking_spot_records + garbage_spot_records + security_records + min_lot_frontage_records +
+        min_lot_size_records + living_space_records + style_records + number_records + age_records )&.uniq
+      @property_list.each do |record|
+        record.weight_age = _weight_age
+        @property << record
+      end
+      return @property
+    end
+  end
+
+  def calculate_weightage(_weight_age, matching_item, number)
+    _weight_age =   matching_item.present? ? _weight_age + number : 0
+  end
+end

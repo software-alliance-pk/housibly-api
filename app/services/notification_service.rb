81520aae7977595fcca8451ec0e203cfecfa45be
vs-ror <backend@viralsquare.org>
2022-10-11 00:20:29 +0900

    push file

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -1,6 +1,7 @@
 class NotificationService
     require 'fcm'
     def self.fcm_push_notification_for_chat_messages(recipient,actor,type,user_notification,action,title)
+        puts "<<<<<<<<<<<<<PUSH NOTIFICATIONS<<<<<<<<<<<<<<<<"
         data = {action: user_notification.action,title: user_notification.title,type: user_notification.type, event_const: type ,recipient: recipient, sender:actor, avatar: actor&.avatar&.url, conversation_id: user_notification.conversation_id}
         fcm_client = FCM.new('AAAAyBDcdag:APA91bFtIo0jPppavG5gExCfcRJMsMvnzJTENiBscXdM6P86rOsrVgF1kH-rI9gSYkpcShtvpukhZlR8G9aK9pC7cTw8C0L_dFEMT4thE_KK0g7rPlz7JUCDO1AU3mF2778JnShuUMzs') # set your FCM_SERVER_KEY
 

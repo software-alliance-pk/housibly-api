3f5ec5a5a18f5046747c4b425127c2740f8e3a4d
vs-ror <backend@viralsquare.org>
2022-10-18 21:16:31 +0900

    support conversation create

--- app/controllers/api/v1/supports_controller.rb
+++ app/controllers/api/v1/supports_controller.rb
@@ -2,6 +2,15 @@ class Api::V1::SupportsController < Api::V1::ApiController
   def create_ticket
     @ticket = current_user.supports.new(support_params.merge(ticket_number: generate_ticket_number.upcase))
     if @ticket.save
+      conv_type = current_user.want_support_closer? ? "support_closer" : "end_user"
+      @conversation = @ticket.
+        build_support_conversation(recipient_id: Admin.admin.first.id,sender_id:@current_user.id,
+                                   conv_type: conv_type)
+      if @conversation.save
+        @current_user.user_support_messages.build(body: @ticket.description, image: support_params[:image], support_conversation_id: @conversation.id).save
+      else
+        render_error_messages(@conversation)
+      end
       AdminNotification.create(actor_id: Admin.admin.first.id,
                               recipient_id: current_user.id, action: "#{current_user.full_name} generated new ticket") if Admin&.admin.present?
       @ticket

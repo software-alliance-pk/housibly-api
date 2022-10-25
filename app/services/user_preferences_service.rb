568aef27d348c8f630f43bd88fba12c49607d15d
vs-ror <backend@viralsquare.org>
2022-10-25 21:26:57 +0900

    bookmark

--- app/services/user_preferences_service.rb
+++ app/services/user_preferences_service.rb
@@ -193,30 +193,30 @@ class UserPreferencesService
   def bookmark_match_property(user)
     _weight_age = 0
     user_preference =  user.user_preference
-    @property = Property.joins(:bookmarks).where("bookmarks.user_id=?", user.id)
-    property_type_records = property.where("properties.type ILIKE ? ", "%#{user_preference.property_type}%")
+    property = Property.joins(:bookmarks).where("bookmarks.user_id=?", user.id)
+    property_type_records = property.where("properties.type ILIKE ? ", "%#{user_preference&.property_type}%")
     if property_type_records.present?
-      property_type_records = @property.where("properties.type ILIKE ? ", "%#{user_preference.property_type}%")
+      property_type_records = property.where("properties.type ILIKE ? ", "%#{user_preference&.property_type}%")
       _weight_age = _weight_age + 10  if property_type_records.present?
 
-      price_records  = @property.price_matcher(user_preference.min_price,user_preference.max_price)
+      price_records  = property.price_matcher(user_preference.min_price,user_preference.max_price)
       _weight_age = _weight_age + 10  if  price_records.present?
 
-      bath_rooms_records = @property.bath_rooms_matcher(user_preference.min_bathrooms,user_preference.max_bathrooms)
+      bath_rooms_records = property.bath_rooms_matcher(user_preference.min_bathrooms,user_preference.max_bathrooms)
       _weight_age = _weight_age + 10  if bath_rooms_records.present?
 
 
-      bed_rooms_records = @property.bed_rooms_matcher(user_preference.min_bedrooms,user_preference.max_bedrooms)
+      bed_rooms_records = property.bed_rooms_matcher(user_preference.min_bedrooms,user_preference.max_bedrooms)
       _weight_age = _weight_age + 10 if bed_rooms_records.present?
 
       # parking_spot_records = Property.property_parking_spot(user_preference.parking_spot,user_preference.parking_spot)
       # _weight_age = _weight_age + 7.5 if parking_spot_records.present?
 
-      garbage_spot_records = @property.property_garage(user_preference.garbage_spot)
+      garbage_spot_records = property.property_garage(user_preference.garbage_spot)
       _weight_age = _weight_age + 10 if garbage_spot_records.present?
 
 
-      security_records = @property.property_security(user_preference.security)
+      security_records = property.property_security(user_preference.security)
       _weight_age = _weight_age + 10 if security_records.present?
 
       # min_lot_frontage_records = Property.property_min_lot_frontage(user_preference.min_lot_frontage)
@@ -231,16 +231,16 @@ class UserPreferencesService
       # _weight_age = _value if _value.present?
 
 
-      style_records = @property.property_style_matcher(user_preference.property_style)
+      style_records = property.property_style_matcher(user_preference.property_style)
       _weight_age = _weight_age + 10 if style_records.present?
 
-      number_records = @property.property_total_number_of_rooms(user_preference.min_living_space,user_preference.max_living_space)
+      number_records = property.property_total_number_of_rooms(user_preference.min_living_space,user_preference.max_living_space)
       _weight_age = _weight_age + 10 if number_records.present?
 
-      age_records = @property.property_age(user_preference.max_age)
+      age_records = property.property_age(user_preference.max_age)
       _weight_age = _weight_age + 10 if age_records.present?
 
-      type_records = @property.property_type_matcher_2(user_preference.property_type)
+      type_records = property.property_type_matcher_2(user_preference.property_type)
       _weight_age = _weight_age + 10 if type_records.present?
 
       #living_space_records
@@ -253,6 +253,7 @@ class UserPreferencesService
       end
       return @property
     else
+        @property = Property.joins(:bookmarks).where("bookmarks.user_id=?", user.id)
       return @property
     end
   end

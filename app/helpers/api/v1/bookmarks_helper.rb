7b01d52dd9783fe1fce57a8618e8842cc1527398
vs-ror <backend@viralsquare.org>
2022-10-29 02:26:06 +0900

    &

--- app/helpers/api/v1/bookmarks_helper.rb
+++ app/helpers/api/v1/bookmarks_helper.rb
@@ -6,10 +6,10 @@ module Api::V1::BookmarksHelper
 		else
   			  _user_preference = user.user_preference
   			  if _user_preference.present?
-  			  	   _weight_age = _weight_age + 30 if property.type == _user_preference.property_type
-  			  	   _weight_age = _weight_age + 30 if property.price >= _user_preference&.min_price && property.price <= _user_preference&.max_price
-  			  	   _weight_age = _weight_age + 20 if property.bath_rooms >= _user_preference&.min_bathrooms &&  property.bath_rooms <= _user_preference&.max_bathrooms
-  			  	   _weight_age = _weight_age + 20 if property.bed_rooms >= _user_preference&.min_bedrooms && property.bed_rooms <= _user_preference&.max_bedrooms
+  			  	   _weight_age = _weight_age + 30 if property&.type == _user_preference.property_type
+  			  	   _weight_age = _weight_age + 30 if property&.price >= _user_preference&.min_price && property&.price <= _user_preference&.max_price
+  			  	   _weight_age = _weight_age + 20 if property&.bath_rooms >= _user_preference&.min_bathrooms &&  property&.bath_rooms <= _user_preference&.max_bathrooms
+  			  	   _weight_age = _weight_age + 20 if property&.bed_rooms >= _user_preference&.min_bedrooms && property&.bed_rooms <= _user_preference&.max_bedrooms
   			  else
   			  	_weight_age
   			  end

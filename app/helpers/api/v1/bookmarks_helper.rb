dd6fb36e4c96c237e3bf5e556441f6ada411dc27
vs-ror <backend@viralsquare.org>
2022-10-29 02:02:18 +0900

    fixed

--- app/helpers/api/v1/bookmarks_helper.rb
+++ app/helpers/api/v1/bookmarks_helper.rb
@@ -6,12 +6,12 @@ module Api::V1::BookmarksHelper
 		else
   			  _user_preference = user.user_preference
   			  if _user_preference.present?
-  			  	   _weight_age = _weight_age + 30 if property.type == user_preference.property_type
-  			  	   _weight_age = _weight_age + 30 if proprty.price >= user_preference.min_price && property.price <= user_preference.max_price
-  			  	   _weight_age = _weight_age + 20 if proprty.bath_rooms && property.min_bathrooms <= user_preference.max_bathrooms
-  			  	   _weight_age = _weight_age + 20 if proprty.bed_rooms && property.min_bedrooms <= user_preference.max_bedrooms
+  			  	   _weight_age = _weight_age + 30 if property.type == _user_preference.property_type
+  			  	   _weight_age = _weight_age + 30 if proprty.price >= _user_preference.min_price && property.price <= _user_preference.max_price
+  			  	   _weight_age = _weight_age + 20 if proprty.bath_rooms && property.min_bathrooms <= _user_preference.max_bathrooms
+  			  	   _weight_age = _weight_age + 20 if proprty.bed_rooms && property.min_bedrooms <= _user_preference.max_bedrooms
   			  else
-  			  	_weight_age = 0
+  			  	_weight_age
   			  end
 		end
 	end

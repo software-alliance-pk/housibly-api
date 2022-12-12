b2e1affe37bf4e3a1bd51e451320545532aeea82
ror-vs <backend@viralsquare.org>
2022-12-12 16:39:40 +0500

    Remove the wss from the production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -131,6 +131,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_cable.disable_request_forgery_protection = true
-  config.action_cable.url = 'wss://hou-sibly.herokuapp.com/cable'
+  config.action_cable.url = 'ws://hou-sibly.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 end

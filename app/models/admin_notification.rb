38e536c0625e02455b100e45d0a725100e5bf548
ror-vs <backend@viralsquare.org>
2023-05-18 17:59:54 +0500

    Commented the code of FCM notification

--- app/models/admin_notification.rb
+++ app/models/admin_notification.rb
@@ -2,9 +2,11 @@ class AdminNotification < Notification
   belongs_to :actor, class_name: 'Admin', foreign_key: :actor_id
   belongs_to :recipient, class_name: 'User',foreign_key: :recipient_id
   after_create :push_notification
-	def push_notification
-    NotificationService.fcm_push_notification_for_complete_profile(recipient,actor,self.type,self,self.action) 
-  end
+
+	# def push_notification
+  #   NotificationService.fcm_push_notification_for_complete_profile(recipient,actor,self.type,self,self.action) 
+  # end
+  
 scope :user_create, -> { where('(action = (?) or action = (?)) and read_at  IS NULL','New User Created','New Support Closer Created') }
 scope :active_deactive, -> { where("(action ILIKE ? OR action  ILIKE ?) and read_at  IS NULL", "% active%", "%deactive%")}
 scope :ticket, -> { where("action ILIKE ? and read_at  IS NULL", "% generated %")}

eb729ea139246c8ab5e2b074d887b3668903269f
umairchd <umairchaudhary6161@gmail.com>
2023-05-19 17:08:50 +0500

    fixing token issue

--- lib/authenticate_helper.rb
+++ lib/authenticate_helper.rb
@@ -1,21 +1,21 @@
 module AuthenticateHelper
   def authenticate_user
-    # email = JsonWebTokenService.decode(request.headers["HTTP_AUTH_TOKEN"])["email"] rescue nil
-    # phone = JsonWebTokenService.decode(request.headers["HTTP_AUTH_TOKEN"])["phone_number"] rescue nil
-    # @current_user = User.find_by(email: email) if email.present?
-    # @current_user = User.find_by(phone_number: phone) if phone.present?
-    # render json: { message: "not authorized" }, status: 401 unless @current_user
+    email = JsonWebTokenService.decode(request.headers["HTTP_AUTH_TOKEN"])["email"] rescue nil
+    phone = JsonWebTokenService.decode(request.headers["HTTP_AUTH_TOKEN"])["phone_number"] rescue nil
+    @current_user = User.find_by(email: email) if email.present?
+    @current_user = User.find_by(phone_number: phone) if phone.present?
+    render json: { message: "not authorized" }, status: 401 unless @current_user
 
-    header = request.headers['Authorization']
-    header = header.split(' ').last if header
-    begin
-      @decoded = JsonWebTokenService.decode(header)
-      @current_user = User.find_by_id(@decoded[:user_id]) || User.find_by_email(@decoded[:email])
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { message: e.message }, status: :unauthorized
-    rescue JWT::DecodeError => e
-      render json: { message: e.message }, status: :unauthorized
-    end
+    # header = request.headers['Authorization']
+    # header = header.split(' ').last if header
+    # begin
+    #   @decoded = JsonWebTokenService.decode(header)
+    #   @current_user = User.find_by_id(@decoded[:user_id]) || User.find_by_email(@decoded[:email])
+    # rescue ActiveRecord::RecordNotFound => e
+    #   render json: { message: e.message }, status: :unauthorized
+    # rescue JWT::DecodeError => e
+    #   render json: { message: e.message }, status: :unauthorized
+    # end
   end
 
   def user_sign_in?

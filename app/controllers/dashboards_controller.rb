acba2c9fd987a6ec5a04f0983e4288399e71b526
Usman879 <usmannoor519@gmail.com>
2023-12-29 16:51:54 +0500

    Changes for Date Range CSV downloads and cancel

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,14 +3,16 @@ class DashboardsController < ApplicationController
     notification = AdminNotification.find_by(id: params[:id])
     notification.update(read_at: Time.now) if notification.present?
     @new_users = User.new_users.order(created_at: :desc).paginate(page: params[:page], per_page: 3)
-    @new_users = User.where(" Date(created_at) >= ? AND Date(created_at) <= ?", params[:start_date], params[:end_date]).paginate(page: params[:page], per_page: 3) if params[:start_date].present? && params[:end_date].present?
+  
+    @start_date = params[:start_date].to_date if params[:start_date].present?
+    @end_date = params[:end_date].to_date if params[:end_date].present?
+    @new_users = User.new_users.where("created_at >= ? AND created_at <= ?", @start_date.beginning_of_day, @end_date.end_of_day).paginate(page: params[:page], per_page: 3) if @start_date.present? && @end_date.present?   
+    @new_users = User.new_users.where(id: params[:checkbox_value].split(",")) if params[:checkbox_value].present? && request.format.csv?
+    
     respond_to do |format|
       format.html
-      @new_users = User.new_users.where(id: params[:checkbox_value].split(",")) if params[:checkbox_value].present?
       format.csv { send_data @new_users ? @new_users.to_csv : "No Data" }
     end
   end
 
-  def notification_page
-  end
 end

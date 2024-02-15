5d8a74e5c8d783bfcb4d07481ed7c78217d60353
Usman879 <usmannoor519@gmail.com>
2024-02-15 18:48:40 +0500

    Seen changes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,5 +3,4 @@ class Notification < ApplicationRecord
   belongs_to :actor, class_name: 'User', foreign_key: :actor_id
   belongs_to :conversation, optional:true
 
-  scope :unread, -> { where(read_at: nil)}
 end

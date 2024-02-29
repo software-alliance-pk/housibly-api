e14ff609e9cfd894749cd123ef453ac505188c30
Muttahhar <58345047+Muttahhar@users.noreply.github.com>
2024-02-29 19:27:10 +0500

    bug fix in property buy/sell notifications. update notification logic. fix total user count in searched address users api.

--- app/jobs/user_preferences_notification_job.rb
+++ app/jobs/user_preferences_notification_job.rb
@@ -25,11 +25,13 @@ class UserPreferencesNotificationJob < ApplicationJob
     seller_ids = []
     notifications_sent = 0
 
+    user_setting = user.user_setting
+
     loop do
       properties = PropertiesSearchService.search_by_preference(user.user_preference.attributes, page_info, user.id)
       puts "ppppppppppppppppppppppppp #{properties.length} "
 
-      notifications_sent = send_notifications(properties, seller_ids, notifications_sent)
+      notifications_sent = send_notifications(user, user_setting, properties, seller_ids, notifications_sent)
 
       break if notifications_sent == 4 || properties.length < page_info[:per_page]
       page_info[:page] += 1
@@ -39,37 +41,50 @@ class UserPreferencesNotificationJob < ApplicationJob
   def notify_for_dream_addresses(user)
     seller_ids = []
     notifications_sent = 0
+    user_setting = user.user_setting
 
     user.dream_addresses.each do |dream_address|
       properties = PropertiesSearchService.search_in_circle(dream_address.slice(:latitude, :longitude), 5, nil, user.id)
-      notifications_sent = send_notifications(properties, seller_ids, notifications_sent)
+      notifications_sent = send_notifications(user, user_setting, properties, seller_ids, notifications_sent)
       break if notifications_sent == 4
     end
   end
 
   private
 
-    def send_notifications(properties, seller_ids, notifications_sent)
+    def send_notifications(user, user_setting, properties, seller_ids, notifications_sent)
       properties.each do |property|
-        break if notifications_sent == 4
-        next if UserNotification.exists?(property_id: property.id, recipient_id: user.id)
-        notifications_sent += 1
+        sent = false
 
         # For BUYER
-        UserNotification.create(
-          actor_id: property.user_id, recipient_id: user.id, property_id: property.id,
-          action: "#{property.user.full_name} is selling properties that you might like",
-          title: property.title, event_type: "buy_property"
-        )
+        if user_setting.push_notification && user_setting.inapp_notification &&
+          !UserNotification.exists?(property_id: property.id, recipient_id: user.id, event_type: 'buy_property')
+
+          UserNotification.create(
+            actor_id: property.user_id, recipient_id: user.id, property_id: property.id,
+            action: "#{property.user.full_name} is selling properties that you might like",
+            title: property.title, event_type: 'buy_property'
+          )
+          sent = true
+        end
 
         # For SELLER
         next if seller_ids.include?(property.user_id)
         seller_ids << property.user_id
-        UserNotification.create(
-          actor_id: user.id, recipient_id: property.user_id, property_id: property.id,
-          action: "#{user.full_name} wants to search for a house that you are selling",
-          title: user.full_name, event_type: "sell_property"
-        )
+
+        if property.user.user_setting.push_notification && property.user.user_setting.inapp_notification &&
+          !UserNotification.exists?(property_id: property.id, actor_id: user.id, event_type: 'sell_property')
+
+          UserNotification.create(
+            actor_id: user.id, recipient_id: property.user_id, property_id: property.id,
+            action: "#{user.full_name} wants to search for a house that you are selling",
+            title: user.full_name, event_type: 'sell_property'
+          )
+          sent = true
+        end
+
+        notifications_sent += 1 if sent
+        break if notifications_sent == 4
       end
 
       notifications_sent

ee536d46639446f4b8cbc8b2b7666ac731e470c1
Aqsa <aqsajabbar7689@gmail.com>
2024-07-22 11:00:18 +0500

    error handling in authentication

--- lib/authenticate_helper.rb
+++ lib/authenticate_helper.rb
@@ -1,11 +1,35 @@
 module AuthenticateHelper
   def authenticate_user
-    email = JsonWebTokenService.decode(request.headers["HTTP_AUTH_TOKEN"])["email"] rescue nil
-    phone = JsonWebTokenService.decode(request.headers["HTTP_AUTH_TOKEN"])["phone_number"] rescue nil
-    @current_user = User.find_by(email: email) if email.present?
-    @current_user = User.find_by(phone_number: phone) if phone.present?
-    render json: { message: "not authorized" }, status: 401 unless @current_user
-
+    # email = JsonWebTokenService.decode(request.headers["HTTP_AUTH_TOKEN"])["email"] rescue nil
+    # phone = JsonWebTokenService.decode(request.headers["HTTP_AUTH_TOKEN"])["phone_number"] rescue nil
+    # @current_user = User.find_by(email: email) if email.present?
+    # @current_user = User.find_by(phone_number: phone) if phone.present?
+    # render json: { message: "not authorized" }, status: 401 unless @current_user
+    begin
+      decoded_token = JsonWebTokenService.decode(request.headers["HTTP_AUTH_TOKEN"])
+    rescue JWT::DecodeError => e
+      render json: { message: "JWT decode error: #{e.message}" }, status: :unauthorized
+      return
+    rescue JWT::VerificationError => e
+      render json: { message: "JWT verification error: #{e.message}" }, status: :unauthorized
+      return
+    rescue StandardError => e
+      render json: { message: "Unexpected error: #{e.message}" }, status: :unauthorized
+      return
+    end
+    
+    email = decoded_token["email"]
+    phone = decoded_token["phone_number"]
+    
+    if email.present?
+      @current_user = User.find_by(email: email)
+    elsif phone.present?
+      @current_user = User.find_by(phone_number: phone)
+    end
+    
+    if @current_user.nil?
+      render json: { message: "User not found for the provided credentials" }, status: :unauthorized
+    end
     # header = request.headers['Authorization']
     # header = header.split(' ').last if header
     # begin
